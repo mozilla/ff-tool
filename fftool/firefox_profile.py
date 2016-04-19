00aee002d22586c5993f70331180bd73ad0bccbc
Richard Pappalardo <rpappalax@gmail.com>
2016-04-18 17:06:05 -0700

    print profile dir

--- fftool/firefox_profile.py
+++ fftool/firefox_profile.py
@@ -53,10 +53,10 @@ def prefs_paths(application, test_type, env='stage'):
 
 
 def clean_profiles():
-
+    print('BASE_PROFILE_DIR:' + BASE_PROFILE_DIR)
     os.remove(os.path.join(BASE_PROFILE_DIR, "profiles.ini"))
-    BASE_PROFILE_DIR =os.path.join(BASE_PROFILE_DIR, "Profiles") 
-    shutil.rmtree(BASE_PROFILE_DIR, True)
+    profile_dir = os.path.join(BASE_PROFILE_DIR, "Profiles") 
+    shutil.rmtree(profile_dir, True)
 
 
 def create_mozprofile(profile_dir, application=None, test_type=None, env=None):
